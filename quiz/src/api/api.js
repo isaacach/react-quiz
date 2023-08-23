4eaa75e76feb1ec5aa2d375aa8abae8eb0961c04
isaacach <124728947+isaacach@users.noreply.github.com>
2023-08-22 22:40:00 -0500

    added get quiz function to filter with user params

--- quiz/src/api/api.js
+++ quiz/src/api/api.js
@@ -10,3 +10,18 @@ export const getDefaultQuiz = async () => {
     const data = await response.json();
     console.log(data.results);
 }
+
+export const getQuizWithParams = async (amount, category, difficulty) => {
+    if (amount === undefined) {
+        amount = 10;
+    }
+    if (category === undefined) {
+        category = 9;
+    }
+    if (difficulty === undefined) {
+        difficulty = 'easy';
+    }
+    const response = await fetch(`https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}`);
+    const data = await response.json();
+    console.log(data.results);
+}
