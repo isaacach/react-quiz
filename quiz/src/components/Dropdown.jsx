96415de1255ffc3dc9bdf889c4969e942fe9d3d8
isaacach <124728947+isaacach@users.noreply.github.com>
2023-08-22 22:20:15 -0500

    added dropdown styles

--- quiz/src/components/Dropdown.jsx
+++ quiz/src/components/Dropdown.jsx
@@ -33,7 +33,7 @@ export default function Dropdown({ options, value, onChange}) {
 
   const renderedOptions = options.map((option) => {
     return (
-      <Panel className='bg-gray-50 border-b' key={option.value} onClick={() => handleOptionClick(option)}>
+      <Panel className='option' key={option.value} onClick={() => handleOptionClick(option)}>
         {option.label}
       </Panel>
     );
@@ -41,11 +41,11 @@ export default function Dropdown({ options, value, onChange}) {
 
   return (
     <div ref={divEl} className='w-52 dropdown'>
-      <Panel className='flex justify-between items-center bg-gray-200  border border-gray-400' onClick={handleClick} >
+      <Panel className='dropdown-panel flex justify-between items-center bg-gray-200  border border-gray-400' onClick={handleClick} >
         {value?.label || 'Select a color'}
         <span>{isOpen ? <HiOutlineChevronUp /> : <HiOutlineChevronDown />}</span>
         </Panel>
-      {isOpen && <div>{renderedOptions}</div>}
+      {isOpen && <div className='options-wrapper'>{renderedOptions}</div>}
     </div>
     
   );
--- quiz/src/components/Panel.jsx
+++ quiz/src/components/Panel.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 import classNames from 'classnames';
 
 export default function Panel({ children , className, ...rest}) {
-  const classes = classNames('border rounded p-3 shadow w-full cursor-pointer', className);
+  const classes = classNames('panel', className);
 
   return (
     <div className={classes} {...rest}>
