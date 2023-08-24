d1cc3ab62a331e84bebd64b652a1c6b1507b49b3
isaacach <124728947+isaacach@users.noreply.github.com>
2023-08-24 12:49:01 -0500

    added colors to result cards depending on if correct

--- quiz/src/components/Results.jsx
+++ quiz/src/components/Results.jsx
@@ -6,8 +6,9 @@ export default function Results({ answers }) {
     <h1>Results</h1>
     <div className="results">
     {answers.map((answer) => {
+      let isCorrect = answer.userAnswer == answer.correctAnswer;
       return (
-        <div key={answer.id} className="result-card">
+        <div key={answer.id} className={`result-card ${isCorrect ? 'green' : 'red'}`}>
           <h3>{answer.question}</h3>
           <p>Your answer: {answer.userAnswer}</p>
           <p>Correct answer: {answer.correctAnswer}</p>
